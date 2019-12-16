31feb4d44e3f1722f27c3852bf97512963774e23
Clarisse Moraes <clarissem95@live.com>
2019-12-16 13:31:10 -0500

    Search Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // JavaScript Sheet
 AOS.init();
 
-$("#searchBtn").on("click", function(){
+$("#searchBtn").on("click", function(event){
+event.preventDefault()
 document.location.href = "recipe.html";
 let textValue = $("#textValue").val(); 
 localStorage.setItem("searchValue", textValue)
