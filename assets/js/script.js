0c4df4551ff9fd3d89c5c3ffd7d3147c55af4208
Craig Gant <craigkgant@gmail.com>
2019-12-17 12:56:46 -0500

    added modal search on keypress

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,11 @@ document.location.href = "recipe.html";
 let textValue = $("#textValue").val(); 
 localStorage.setItem("searchValue", textValue)
 });
+
+$("#Modal1").keypress(function(e){
+    if(e.which == 13) {
+       let textValue = $("#textValue").val().trim(); 
+       localStorage.setItem("searchValue", textValue);
+       document.location.href = "recipe.html";
+   }
+});
